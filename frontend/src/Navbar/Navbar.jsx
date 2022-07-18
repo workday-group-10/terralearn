f8528223cf06c9266aa7a03415d196290d42d333
Evans Asuboah <easuboah@stetson.edu>
2022-07-18 15:01:51 -0600

    Navbar

--- frontend/src/Navbar/Navbar.jsx
+++ frontend/src/Navbar/Navbar.jsx
@@ -1,18 +1,30 @@
-import NavLinks from "../NavLinks/NavLinks";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-export default function Navbar() {
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import './Navbar.css'
+import { Avatar } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import globe from '../assets/globe.png'
+function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="content">
-        <div className="logo">
-          <Link to="/">
-          </Link>
-        </div>
-        <div className="resources">
-          <NavLinks />
+    <div className="Navbar">
+      <div className="Navbar_logo">
+      <Avatar alt="Guest" src={globe} />
+      </div>
+     
+      <div className="Navbar_search">
+     
+        <input className="Navbar_search_input" type="text" />
+           {<SearchIcon className="Navbar_searchIcon"/>}
+        {/*Logo*/}
+      </div>
+      <div className="Navbar_nav">
+        <div className="Navbar_option">
+          <span className="Navbar_optionLineOne">Hello Guest</span>
+          <span className="Navbar_optionLineTwo">Sign In</span>
         </div>
       </div>
-    </nav>
-  );
+    </div>
+  )
 }
+
+export default Navbar
