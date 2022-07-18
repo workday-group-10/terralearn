207f539f2f1874e55fe6cdda7f699bbedfbe2907
Evans Asuboah <easuboah@stetson.edu>
2022-07-18 14:23:31 -0600

    Navbar Done

--- frontend/src/Navbar/Navbar.jsx
+++ frontend/src/Navbar/Navbar.jsx
@@ -1,18 +1,30 @@
-import NavLinks from "../NavLinks/NavLinks";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-export default function Navbar() {
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import './Navbar.css'
+import { Avatar } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import globe from '../assets/globe.png'
+function Header() {
   return (
-    <nav className="navbar">
-      <div className="content">
-        <div className="logo">
-          <Link to="/">
-          </Link>
-        </div>
-        <div className="resources">
-          <NavLinks />
+    <div className="header">
+      <div className="header_logo">
+      <Avatar alt="Guest" src={globe} />
+      </div>
+     
+      <div className="header_search">
+     
+        <input className="header_search_input" type="text" />
+           {<SearchIcon className="header_searchIcon"/>}
+        {/*Logo*/}
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
         </div>
       </div>
-    </nav>
-  );
+    </div>
+  )
 }
+
+export default Header
