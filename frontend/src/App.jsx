9917d280250a40ed771faa7e79d42d509f8c177c
Evans Asuboah <easuboah@stetson.edu>
2022-07-18 12:02:04 -0600

    Fixed the App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,13 @@ import './App.css'
 import * as React from 'react'
 import Navbar from './Navbar/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-
-
-
 function App() {
 
 
   return (
     <div className="App">
        <React.Fragment>
+
        <BrowserRouter>
        <main>
         <Navbar/>
