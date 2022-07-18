60cb2139f8807848a899ab339896cddf1d85acee
AndrewValerio <andrewvalerio1396@gmail.com>
2022-07-18 11:09:16 -0700

    removed some comments

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -19,13 +19,7 @@ function App() {
        <main>
         <Navbar/>
         <Routes>
-            {/* <Route path ="/" element = {<LandingPage/>}/>
-            <Route path ="/activity" element = {<Activity nutritionItems={nutritionItems} setNutritionItems={setNutritionItems} exer={exer} setExer={setExer} loggedIn={loggedIn} setRedirect={setRedirect} setRedirectInfo={setRedirectInfo} appState={appState}/>}/>
-            <Route path ="/exercise" element = {<Exercise loggedIn={loggedIn} setRedirect={setRedirect} setRedirectInfo={setRedirectInfo} appState={appState}/>}/>
-            <Route path ="/nutrition" element = {<Nutrition nutritionItems={nutritionItems} setNutritionItems={setNutritionItems} loggedIn={loggedIn} setRedirect={setRedirect} setRedirectInfo={setRedirectInfo} appState={appState}/>}/>
-            <Route path ="/nutrition/create" element = {<CreateNutrition nutritionItems={nutritionItems} loggedIn={loggedIn} setRedirect={setRedirect} setRedirectInfo={setRedirectInfo} appState={appState}/>}/>
-            <Route path ="/sleep" element = {<Sleep loggedIn={loggedIn} setRedirect={setRedirect} setRedirectInfo={setRedirectInfo} appState={appState}/>}/>
-            <Route path="*" element={<NotFound/>}></Route> */}
+            {/* <Route path ="/" element = {<LandingPage/>}/> */}
             <Route path="/login" element={<Login setAppState={setAppState} redirect={redirect} redirectInfo={redirectInfo} loggedIn={loggedIn} setLoggedIn={setLoggedIn} setRedirect={setRedirect} setRedirectInfo={setRedirectInfo} appState={appState}/>} />
             <Route path="/register" element={<Register setRedirect={setRedirect} setRedirectInfo={setRedirectInfo} setAppState={setAppState} loggedIn={loggedIn} setLoggedIn={setLoggedIn} appState={appState}/>} />
 
