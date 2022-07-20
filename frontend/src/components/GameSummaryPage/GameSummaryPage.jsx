a6e1fb552a5f56368a897cb0c1a4c2e1735ba919
Evans Asuboah <easuboah@stetson.edu>
2022-07-20 15:52:16 -0600

    Worked on the buttons

--- frontend/src/components/GameSummaryPage/GameSummaryPage.jsx
+++ frontend/src/components/GameSummaryPage/GameSummaryPage.jsx
@@ -1,12 +1,8 @@
 import "./GameSummaryPage.css";
-<<<<<<< HEAD
-import guessImage from "../assets/guess-image.png";
-import { useNavigate } from "react-router-dom";
-=======
 import React from "react";
 import guessImage from "../assets/guess-image.png"
 import ProgressBar from "@ramonak/react-progress-bar";
->>>>>>> 5c77925ad2adcc5f95a7ad33f1167f3d591164e2
+import { useNavigate } from "react-router-dom";
 export default function GameSummaryPage() {
   const navigate = useNavigate()
   const navigateInstructions=()=>{
@@ -23,7 +19,15 @@ export default function GameSummaryPage() {
       </div>
       <div className="game-summary-image">
         <img src={guessImage} alt="guess" />
-        <ProgressBar completed={70} bgColor='#6495ed' baseBgColor="#385682" labelColor="#FFFFFF" />
+        <div className="TotalPoints">
+          Total Points
+        </div>
+        <ProgressBar className="progressbar" completed={70} bgColor='#6495ed' baseBgColor="#385682" labelColor="#FFFFFF" />
+        <span className="buttons-span">
+          <button className="playAgain-button" onClick={navigateInstructions}>Play Again</button>
+          <button className="mainMenu-button" onClick={navigatePostLanding}>Main Menu</button>
+
+        </span>
         </div>
     </div>
   );
