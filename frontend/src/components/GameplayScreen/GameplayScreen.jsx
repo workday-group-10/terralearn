73817c9ec4fdfd378ec16d1fd47fc8348777f0cd
foloye <foloye11@gmail.com>
2022-07-20 11:43:57 -0700

    added onclick handleer

--- frontend/src/components/GameplayScreen/GameplayScreen.jsx
+++ frontend/src/components/GameplayScreen/GameplayScreen.jsx
@@ -2,17 +2,37 @@ import * as React from "react"
 import "./GameplayScreen.css"
 import PinMap from "../PinMap/PinMap"
 import StreetViewMap from "../StreetViewMap/StreetViewMap"
+import { useNavigate, Link } from "react-router-dom"
 
 export default function GameplayScreen() {
+  const navigate = useNavigate()
+  function navSummary(){
+    navigate("/gameSummary")
+  }
+
   return (
     
     <div className="gameplay-screen">
         <h1>GAMEPLAYSCREEN</h1>
-        <StreetViewMap/>
+        <div className="round-div">
+          <div className="round-text">
+            <span className="title-round">Round</span><br/>
+            <span>1/1</span>
+          </div>
+          <div className="score-text">
+            <span className="title-round">Score</span><br/>
+            <span>0</span>
+          </div>
+          
+        </div>
+        <div className="street-div">
+          <StreetViewMap/>
+        </div>
+        
         <div className="user-interact">
             <div className="guess-div">
                 <h2 className="game-instruct">Guess the location on the map!</h2>
-                <button className="guess-btn">Guess!</button>
+                <button className="guess-btn" onClick={navSummary}>Guess!</button>
             </div>
             
             <PinMap/>
