c40f7b1f31c69666d77a11d658f64550d29680b5
AndrewValerio <andrewvalerio1396@gmail.com>
2022-07-20 09:31:04 -0700

    page size fixes and post login landing cssdone

--- frontend/src/components/PostLoginLanding/PostLoginLanding.jsx
+++ frontend/src/components/PostLoginLanding/PostLoginLanding.jsx
@@ -6,27 +6,45 @@ export default function PostLoginLanding(props) {
   props.setNavbarName(props.user.firstName)
   return (
     <div className="home">
+    <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+
+            <div class="bg-animation">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            <div id="stars4"></div>
+            </div>
        <div className="home_container">
-        <Banner />
-        <div className="text_titles">
-          <span className="title1">Let's Explore</span>
-          <span className="title2">The World</span>
-         
+       <div className="post-planet"> 
+        <div className="wrap">
+        <div className="background"></div> 
         </div>
-      
+        <div className="mask"></div>
+        </div>
+        <div className="button-container">
+        <div className="titles">
+            <h2 className="title-one">Learn About </h2>
+            <h1 className="title-two">Your Home</h1>
+            </div>
+            <button className="btn">Play</button>
+        </div>
+      </div>
       <div className="home_row">
       <Category continent="Europe"/>
         <Category continent="Asia"/>
           <Category continent="Africa"/>
         <Category continent="USA"/>
-      
-      </div>
-      <div className="home_row">
-        
-      </div>
-      <div className="home_row">
-      
-      </div>
       </div>
     </div>
   );
