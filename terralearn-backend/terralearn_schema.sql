899e827946ee3d06e2a3386a6e71a4a9ace08773
Evans Asuboah <easuboah@stetson.edu>
2022-07-20 11:08:08 -0600

    worked on the connecting the countdown to the gameplay and also the sign up persistence

--- terralearn-backend/terralearn_schema.sql
+++ terralearn-backend/terralearn_schema.sql
@@ -12,10 +12,6 @@ CREATE TABLE category(
     image_url  TEXT NOT NULL,
     difficulty TEXT NOT NULL
 );
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 CREATE TABLE cities(
     id          SERIAL PRIMARY KEY,
     city        TEXT NOT NULL,
@@ -23,34 +19,6 @@ CREATE TABLE cities(
     FOREIGN KEY(category_id) REFERENCES category(id)
 );
 
-<<<<<<< HEAD
-CREATE TABLE games(
-    id          SERIAL PRIMARY KEY,
-    user_id     INTEGER NOT NULL,
-    category_id INTEGER NOT NULL,
-    guess_id    INTEGER NOT NULL,
-    final_score INTEGER NOT NULL,
-    created_at  DATE NOT NULL DEFAULT CURRENT_DATE,
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(category_id) REFERENCES category(id)
-);
-CREATE TABLE profiles(
-    id           SERIAL PRIMARY KEY,
-    user_id      INTEGER NOT NULL,
-    game_id      INTEGER NOT NULL,
-    recent_score INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(game_id) REFERENCES games(id)
-);
-CREATE TABLE guess(
-    id       SERIAL PRIMARY KEY,
-    location TEXT NOT NULL,
-    link     TEXT NOT NULL,
-    user_id  INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id)
-);
-=======
->>>>>>> main
 CREATE TABLE favorities(
     id          SERIAL PRIMARY KEY,
     user_id      INTEGER NOT NULL,
