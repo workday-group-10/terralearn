0761671dac717b750c7abca2b5982b09bab7d4fe
Evans Asuboah <easuboah@stetson.edu>
2022-07-20 15:39:26 -0600

    Worked on the buttons

--- frontend/src/components/GameSummaryPage/GameSummaryPage.jsx
+++ frontend/src/components/GameSummaryPage/GameSummaryPage.jsx
@@ -1,15 +1,32 @@
 import "./GameSummaryPage.css";
-import guessImage from "../assets/guess-image.png"
+import guessImage from "../assets/guess-image.png";
+import { useNavigate } from "react-router-dom";
 export default function GameSummaryPage() {
+  const navigate = useNavigate()
+  const navigateInstructions=()=>{
+    navigate("/instructions")
+  }
+  const navigatePostLanding=()=>{
+    navigate("/PostLoginlanding")
+  }
   return (
     <div className="game-summary-page">
-        <div className="game-summary-header">
-            <h1>Game Summary</h1>
-            <hr></hr>
-        </div>
-        <div className="game-summary-image">
+      <div className="game-summary-header">
+        <h1>Game Summary</h1>
+        <hr></hr>
+      </div>
+      <div className="game-summary-image">
         <img src={guessImage} alt="guess" />
-        </div>
+      </div>
+      <div className="TotalPoints">
+        Total Points
+
+      </div>
+      <span className="buttons-span">
+        <button className="playAgain-button" onClick={navigateInstructions}>Play Again</button>
+
+        <button className="mainMenu-button" onClick={navigatePostLanding}>Main Menu</button>
+      </span>
     </div>
   );
 }
