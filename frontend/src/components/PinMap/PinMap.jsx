79c5a0b86c623932d857386fe52470fdb2f2b46f
Evans Asuboah <easuboah@stetson.edu>
2022-07-24 08:48:27 -0600

    Commit

--- frontend/src/components/PinMap/PinMap.jsx
+++ frontend/src/components/PinMap/PinMap.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import MapComponent from "../Map/Map"
-import * as React from "react"
-import "./PinMap.css"
-import { useState } from "react"
-
-export default function PinMap() {
-  const [isMarkerShown,setisMarkerShown] = useState(true)
-  return (
-    <div className="pin-map">
-        <h1>PinMap</h1>
-       <MapComponent isMarkerShown = {isMarkerShown }/>
-=======
 
 import * as React from "react"
 import "./PinMap.css"
@@ -27,7 +14,6 @@ export default function PinMap() {
   return (
     <div className="pin-map">
        <MyMapComponent isMarkerShown = {isMarkerShown } positions={positions} setPositions={setPositions}/>
->>>>>>> 4f27e4e333eceb5d57c3aa354767d7d134f535cd
     </div>
   )
 }
