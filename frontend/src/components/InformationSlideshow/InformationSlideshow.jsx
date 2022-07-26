7b8cd2bdd2a0c828c66ae995644905d6a7611903
foloye <foloye11@gmail.com>
2022-07-26 13:04:18 -0700

    idk

--- frontend/src/components/InformationSlideshow/InformationSlideshow.jsx
+++ frontend/src/components/InformationSlideshow/InformationSlideshow.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import axios from "axios";
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import "./InformationSlideshow.css"
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
@@ -8,20 +8,18 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 export default function InformationSlideshow(props) {
     const input = props.location
     const [errors, setErrors] = useState({})
+    const [firstResponse, setFirstReponse] = useState()
 
     const endpoints = "https://en.wikipedia.org/w/api.php?"
     const params = {
         origin: '*',
         format: 'json',
         action: 'query',
-        prop: 'extracts',
+        prop: 'extracts|images',
         exsentences: 5,
         exintro: true,
         generator: 'search',
         explaintext: true,
-        prop: 'imageinfo',
-        iiprop: 'url'|'size',
-        imlimit: 5,
 
     }
 
@@ -33,24 +31,125 @@ export default function InformationSlideshow(props) {
     }
 
     console.log(input)
-    async function getData(){
-        params.gsrsearch = input;
-        if (isInputEmpty){
-            try{
-                const response = await axios.get(endpoints, {params})
-                console.log(endpoints, {params})
-                console.log(response.data.query)
-                // console.log(Object.values(response.data.query.pages)[0])
-            } catch (err) {
-                setErrors({"error": err})
-            }
+    // async function getData(){
+    //     params.gsrsearch = input;
+    //     if (isInputEmpty){
+    //         try{
+    //             console.log("in first try")
+    //             const response = await axios.get(endpoints, {params})
+    //             // console.log(endpoints, {params})
+    //             // console.log(response.data.query)
+    //             setFirstReponse(Object.values(response.data.query.pages)[0])
+    //             //.images[0]).title
+    //             console.log("first response", firstResponse)
+    //             // console.log(Object.values(response.data.query.pages)[0])
+    //         } catch (err) {
+    //             setErrors({"error": err})
+    //         }
+            // try{
+            //     console.log("in second try", response)
+            //     params_two = {
+            //         action: "query",
+            //         format: "json",
+            //         prop: "imageinfo",
+            //         titles: (Object.values(response.data.query.pages)[0].images[0]).title,
+            //         iiprop: "url",
+            //     };
+            //     const imageResponse = await axios.get(endpoints, {params_two})
+            //     console.log("image response", imageResponse)
+
+            // } catch (err){
+            //     setErrors({"error": err})
+            // }
             
 
             
-        }
+    //     }
         
-    }
-    getData();
+    // }
+    params.gsrsearch = input;
+    useEffect(() => {
+        async function getData(){
+            
+            if (isInputEmpty){
+                try{
+                    console.log("in first try")
+                    params.gsrsearch = input;
+                    const response = await axios.get(endpoints, {params})
+                    console.log("first response", response.data)
+                    setFirstReponse(response.data.query.pages)
+                    
+                    
+                } catch (err) {
+                    setErrors({"error": err})
+                }
+                
+            }
+            
+        }
+        getData();
+        console.log("first error", firstResponse)
+      }, [input]);
+    //   console.log("usestate", Object.values(firstResponse)[0])
+    //   useEffect(() => {
+    //     async function getImages(){
+    //         const tempImg = ("image arra", Object.values(firstResponse)[0].images)[0]
+    //         console.log("title of image", tempImg.title)
+    //         // const params_two = {
+    //         //     origin: '*',
+    //         //     format: 'json',
+    //         //     action: 'query',
+    //         //     prop: 'imageinfo',
+    //         //     titles: tempImg.title,
+    //         //     iiprop: "url",
+    //         // };
+    //         // params.gsrsearch = input;
+    //         // try{
+                
+    //         //     console.log("in second try")
+                
+    //         //     const imageResponse = await axios.get(endpoints, {params_two})
+    //         //     console.log("image response", imageResponse)
+
+    //         // } catch (err){
+    //         //     setErrors({"error": err})
+    //         // }
+
+
+    //         var url = "https://en.wikipedia.org/w/api.php"; 
+
+    //         var params2 = {
+    //             action: "query",
+    //             format: "json",
+    //             prop: "imageinfo",
+    //             titles: tempImg
+    //         };
+            
+    //         url = url + "?origin=*";
+    //         Object.keys(params2).forEach(function(key){url += "&" + key + "=" + params2[key];});
+            
+    //         fetch(url)
+    //             .then(function(response){return response.json();})
+    //             .then(function(response) {
+    //                 var pages = response.query.pages;
+    //                 console.log(pages)
+    //                 for (var p in pages) {
+    //                     console.log(pages[p].title);
+    //                 }
+    //             })
+    //             .catch(function(error){console.log(error);});
+
+
+
+
+
+            
+                
+            
+            
+    //     }
+    //     getImages();
+    //   }, [firstResponse]);
 
     
 
