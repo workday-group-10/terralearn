a9ad91a039a8358e43916c4de2c841f807922e2f
AndrewValerio <andrewvalerio1396@gmail.com>
2022-07-25 20:48:35 -0700

    Fixed geoapify call to google maps streetview

--- frontend/src/components/GoogleStreetMaps/GoogleStreetMaps.jsx
+++ frontend/src/components/GoogleStreetMaps/GoogleStreetMaps.jsx
@@ -5,7 +5,15 @@ import "./GoogleStreetMaps.css"
 
 class GoogleMapsStreetView extends React.Component {
   render() {
-    console.log(this.props)
+    console.log("lat/lon in googlemapsstreetview", this.props)
+    const data = this.props;
+    console.log(data)
+    var stringLat = JSON.stringify(data.latitude);
+    var latitude = parseFloat(stringLat);
+    var stringLon = JSON.stringify(data.longitude);
+    var longitude = parseFloat(stringLon);
+    console.log("parseFloat lat", latitude)
+    console.log("parseFloat lon", longitude)
     const mapStyles = {
         width: '100%',
         height: '86.2vh',
@@ -14,7 +22,7 @@ class GoogleMapsStreetView extends React.Component {
  
 
     const streetViewPanoramaOptions = {
-      position: { lat: this.props.latitiude, lng: this.props.longitude },
+      position: { lat: latitude, lng: longitude },
       pov: { heading: 100, pitch: 0 },
       zoom: 1,
       addressControl: false,
