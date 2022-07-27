f59b797a583bf6ef29c0db2aee67245f0e4e8b61
AndrewValerio <andrewvalerio1396@gmail.com>
2022-07-27 11:46:38 -0700

    started the your info feature

--- frontend/src/components/GameplayScreen/GameplayScreen.jsx
+++ frontend/src/components/GameplayScreen/GameplayScreen.jsx
@@ -9,7 +9,8 @@ import axios from "axios";
 import { GEOAPIFY_KEY } from "../constants";
 import LoadingSpinner from "../LoadingPage/LoadingSpinner"
 
-export default function GameplayScreen({location, setLocation, positions ,setPositions, latitude, setLatitude, longitude, setLongitude, country_id}) {
+export default function GameplayScreen({location, setLocation, positions ,setPositions, latitude, setLatitude,
+   longitude, setLongitude, country_id, userPlacesInfo, setCurrInfo, currInfo, setUserPlacesInfo}) {
   const [guessed,setGuessed] = useState(false)
   const {cities, setCities} = useCitiesContext();
   var categorizedCities = [];
@@ -17,16 +18,13 @@ export default function GameplayScreen({location, setLocation, positions ,setPos
   const [isFetching, setIsFetching] = useState(false)
   const [data, setData] = useState([]);
   
-
-  //console.log(cities)
-  //console.log(country_id)
+  // filters city context based on where the user chose their category
   function filterCities(){
   for(var i = 0; i < cities.length; i++){
   if(cities[i].category_id == country_id){
   categorizedCities.push(cities[i])
   }
   }
-  //console.log(categorizedCities)
 }
 
 
@@ -37,44 +35,33 @@ export default function GameplayScreen({location, setLocation, positions ,setPos
       navigate("/gameSummary")
     }
   }
+  //calls all functions on page render
   useEffect(() => {
     setIsFetching(true);
     filterCities();
     fetchData();
     setData([])
+    console.log(userPlacesInfo)
   }, []);
 
+    //API call to geoapify to get places given the place_id from our filtered city context
     const fetchData = async () => {
       var i =  Math.floor(Math.random() *20);
 
       const { data } = await axios.get(`https://api.geoapify.com/v2/places?categories=tourism&filter=place:${categorizedCities[0].place_id}&limit=20&apiKey=${GEOAPIFY_KEY}`)
       setData(data);
+      console.log(data)
       //sets location, latitude, longtitude of place guessed
       setLocation(data.features[i].properties.name);
-      setLatitude(data.features[i].properties.lat)
-      setLongitude(data.features[i].properties.lon)
+      setLatitude(data.features[i].properties.lat);
+      setLongitude(data.features[i].properties.lon);
+      setCurrInfo("https://en.wikipedia.org/wiki/" + data.features[i].properties.address_line1);
+      console.log(currInfo)
+      console.log(location)
+      setUserPlacesInfo(oldArray => [...oldArray, {location, currInfo}])
+      console.log(userPlacesInfo)
       setIsFetching(false)
   }
-
-         //console.log("Latitude:", latitude)
-         //console.log("Longitude:", longitude)
-
-
-    //   if (res?.data) {
-    //     setLatitude(res.data.features[0].properties.lat)
-    //     setLongitude(res.data.features[0].properties.lon)
-    //     console.log("res:", res)
-    //   } else {
-    //     setError("Error fetching products.")
-    //   }
-    // }
-    // catch (err) {
-    //   console.log(err)
-    //   const message = err?.response?.data?.error?.message
-    //   setError(message ?? String(err))
-    // } finally {
-    //   setIsFetching(false)
-    // }
 
   return (
     <div className="gameplay-screen">
@@ -82,7 +69,6 @@ export default function GameplayScreen({location, setLocation, positions ,setPos
       {!isFetching && (
         <div className="google_street">
         <StreetViewMap latitude={latitude} longitude={longitude}/>
-        {/* <StreetViewMap/> */}
         </div>
       )};
         <div className="google_map">
